7f25a6145fe0dd6f2197837d57d597a89e195268
Alendia <81672484+Alendia22@users.noreply.github.com>
2022-10-16 16:37:47 +0700

    Add files via upload

---
+++ latihanapi1.php
@@ -0,0 +1,24 @@
+<?php
+$url = 'https://data.covid19.go.id/public/api/prov.json';
+$data1 = file_get_contents($url);
+$data_covid=json_decode($data1);
+$table = "<h1>Data Covid</h1>";
+$table .= "<table border=1>";
+$table .= "<tr><td>No</td><td>Provinsi</td>;
+			   <td>Sembuh</td><td>Meninggal</td>;
+			   <td>Dirawat</td><td>Jumlah Kasus</td></tr>";
+//echo "<pre>"; print_r($data_covid->list_data);
+for($i=0;$i<count($data_covid->list_data);$i++) {
+		 $no=$i+1;
+		 $table .= "<tr>
+					<td>{$no}</td>
+					<td>{$data_covid->list_data[$i]->key}</td>
+					<td>{$data_covid->list_data[$i]->jumlah_sembuh}</td>
+					<td>{$data_covid->list_data[$i]->jumlah_meninggal}</td>
+					<td>{$data_covid->list_data[$i]->jumlah_dirawat}</td>
+					<td>{$data_covid->list_data[$i]->jumlah_kasus}</td>
+					</tr>";
+					
+}
+$table .= "</table>";
+echo $table;
